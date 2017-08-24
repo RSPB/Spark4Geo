1408aac81eb6a034b23b4f5f4bf125df45ee195b
tracek <lukasz.tracewski@gmail.com>
2017-08-24 15:20:15 +0200

    area of intersection incorrect

--- build.sbt
+++ build.sbt
@@ -5,8 +5,8 @@ version := "1.0"
 scalaVersion := "2.11.11"
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "2.1.0",
+  "org.apache.spark" %% "spark-core" % "2.2.0",
   "org.datasyslab" % "sernetcdf" % "0.1.0",
-  "org.datasyslab" % "geospark" % "0.8.0",
+  "org.datasyslab" % "geospark" % "0.8.1",
   "org.scala-lang.modules" % "scala-java8-compat_2.11" % "0.8.0"
 )
--- src/main/scala/SpatialJoinShp.scala
+++ src/main/scala/SpatialJoinShp.scala
@@ -1,4 +1,5 @@
 import java.util
+import java.io._
 
 import org.apache.log4j.Level
 import org.apache.log4j.Logger
@@ -25,23 +26,66 @@ object SpatialJoinShp extends App {
     polygon
   }
 
+  def getMetadata(polygon: Polygon) = {
+    val metadata = polygon.getUserData.toString.split("\t")
+    val id: Int = metadata(0).toInt
+    val name: String = metadata(1)
+    (id, name)
+  }
+
+  def getIntersection(species: Polygon)(wdpa: Polygon) = {
+    val intersection = wdpa.intersection(species)
+
+    val wdpa_metadata = wdpa.getUserData.toString.split("\t")
+    val wdpa_id: Int = wdpa_metadata(0).toInt
+    val wdpa_name: String = wdpa_metadata(3)
+
+    val species_metadata = species.getUserData.toString.split("\t")
+    val species_id: Int = species_metadata(0).toInt
+    val species_name: String = species_metadata(1)
+
+    (wdpa_id, wdpa_name, species_id, species_name, intersection.getArea)
+  }
+
+  def getIntersection2(species: Polygon)(wdpa: Polygon) = {
+    val intersection = wdpa.intersection(species)
+
+    val wdpa_metadata = wdpa.getUserData.toString.split("\t")
+    val wdpa_id: Int = wdpa_metadata(0).toInt
+    val wdpa_name: String = wdpa_metadata(3)
+
+    val species_metadata = species.getUserData.toString.split("\t")
+    val species_id: Int = species_metadata(0).toInt
+    val species_name: String = species_metadata(1)
+
+    ((wdpa_id, species_id), intersection.getArea)
+  }
+
   Logger.getLogger("org").setLevel(Level.WARN)
   Logger.getLogger("akka").setLevel(Level.WARN)
 
   val conf = new SparkConf().setAppName("SpatialJoinSpeciesPA").setMaster("local[4]")
   val sc = new SparkContext(conf)
 
-  val wdpa = loadShapefile("/home/tracek/Data/Spark4Geo/wdpa/wdpa.shp")
-  val species = loadShapefile("/home/tracek/Data/Spark4Geo/amphib/amphib.shp")
+  val wdpa = loadShapefile("/home/tracek/Data/spark4geo_subset/wdpa/")
+  val species = loadShapefile("/home/tracek/Data/spark4geo_subset/amphib/")
 
   wdpa.spatialPartitioning(GridType.QUADTREE)
   species.spatialPartitioning(wdpa.partitionTree)
 
   val query = JoinQuery.SpatialJoinQuery(wdpa, species, false, false)
 
-  val user_data_sample = JoinQuery.SpatialJoinQuery(wdpa, species, false, false).first()._1.getUserData
-  if (user_data_sample.toString.isEmpty) println("UserData is empty") else println(user_data_sample)
+  // val join_result = query.rdd.map((tuple: (Polygon, util.HashSet[Polygon])) => (tuple._1, tuple._2.asScala.map(tuple._1.intersection(_).getArea)) )
+  // val join_result = query.rdd.map((tuple: (Polygon, util.HashSet[Polygon])) => tuple._2.asScala.map(x => getIntersection2(tuple._1)(x)  ) )
+  val join_result = query.rdd.flatMap((tuple: (Polygon, util.HashSet[Polygon])) => tuple._2.asScala.map(x => getIntersection2(tuple._1)(x)  ) )
+  val merge_results = join_result.reduceByKey(_ + _)
+  val intersections_area = merge_results.collect()
 
-//  val join_result = query.rdd.map((tuple: (Polygon, util.HashSet[Polygon])) => (tuple._1, tuple._2.asScala.map(tuple._1.intersection(_).getArea)) )
-//  val intersections = join_result.collect()
+  val writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/home/tracek/results.txt")))
+
+  for (row <- intersections_area) {
+    writer.write(row._1._1 + "," + row._1._2 + "," + row._2 + "\n")
+  }
+  writer.close()
 }
+
